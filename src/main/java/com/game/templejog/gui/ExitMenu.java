fa839799f2fdeae04266f207e9ece548b401495c
Cynthia Pottin <36019063+cpottin@users.noreply.github.com>
2023-02-15 11:21:30 -0500

    refactored exit AB#1148

--- src/main/java/com/game/templejog/gui/ExitMenu.java
+++ src/main/java/com/game/templejog/gui/ExitMenu.java
@@ -4,28 +4,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ExitMenu {
-    public static void setUpExitGUI(GUI gui) {
-        final JFrame exitFrame = new JFrame("Exit Menu");
-        exitFrame.setLayout(new BorderLayout());
-        final JTextArea exitPrompt = new JTextArea("Are you sure you want to quit?");
-        final JButton exitYes = new JButton("Yes");
-        final JButton exitNo = new JButton("No");
-        exitYes.addActionListener(e -> {
-            if (e.getSource() == exitYes) {
-                exitFrame.dispose();
-                gui.getMainContainer().dispose();
-            }
-        });
-        exitNo.addActionListener(e -> {
-            if (e.getSource() == exitNo) {
-                exitFrame.dispose();
-            }
-        });
-        exitFrame.setBounds(0, 0, GUI.GAME_WIDTH / 4, GUI.GAME_HEIGHT / 4);
-        exitFrame.add(exitPrompt, BorderLayout.PAGE_START);
-        exitFrame.add(exitYes, BorderLayout.LINE_START);
-        exitFrame.add(exitNo, BorderLayout.LINE_END);
-        exitFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        exitFrame.setVisible(true);
+static JFrame frame;
+    public static void setUpExitOptions(GUI gui){
+
+        //click event will return an int and we can use that int in the if statement to take action
+        int n = JOptionPane.showConfirmDialog(
+                frame,
+                "Are you sure you want to exit?",
+                "Exit Question",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE
+        );
+        if(n == 0){
+            gui.getMainContainer().dispose();
+        }
     }
 }
