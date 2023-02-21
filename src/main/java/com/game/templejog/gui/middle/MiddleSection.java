dfd08b003482aba7d2056e1b11c13589a6b6a047
Bryce Meadors <bryce.meadors@gmail.com>
2023-02-21 09:14:05 -0800

    Added enemy encounter to images. AB#1197.

--- src/main/java/com/game/templejog/gui/middle/MiddleSection.java
+++ src/main/java/com/game/templejog/gui/middle/MiddleSection.java
@@ -10,7 +10,9 @@ import com.game.templejog.gui.MainContainer;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 import java.util.*;
 import java.util.List;
@@ -20,16 +22,17 @@ public class MiddleSection {
     private JPanel middleSectionPanel;
     private JPanel imagePanel;
     private final JPanel buttonPanel;
-    private JLabel imageLabel;
+    private JLabel backgroundImageJLabel;
     private ImageIcon currentLocationBackgroundIcon;
     private JButton upButton, downButton, leftButton, rightButton;
     private JLabel encounterLabel;
+    private ImageIcon currentLocationEncounterIcon;
 
     public MiddleSection(Game game) {
         this.game = game;
         this.middleSectionPanel = new JPanel();
         this.imagePanel = new JPanel();
-        this.imageLabel = new JLabel();
+        this.backgroundImageJLabel = new JLabel();
         // Create the button panel
         this.buttonPanel = new JPanel(new BorderLayout());
         this.leftButton = new JButton("<");
@@ -87,30 +90,28 @@ public class MiddleSection {
     }
 
     public JPanel setUpMiddleSectionJPanel() {
-        ImageIcon getEncounterImage = getEncounterImageTo();
-        if (getEncounterImage != null) {
-            encounterLabel.setIcon(getEncounterImage);
-        }
-
-        leftButton.setLocation(this.imagePanel.getX() - 10, this.imagePanel.getY());
         buttonPanel.setLayout(new BorderLayout());
         buttonPanel.add(leftButton, BorderLayout.WEST);
         buttonPanel.add(rightButton, BorderLayout.EAST);
         buttonPanel.add(upButton, BorderLayout.NORTH);
         buttonPanel.add(downButton, BorderLayout.SOUTH);
-        encounterLabel.setIcon(getEncounterImage);
 
-        imagePanel.add(imageLabel, BorderLayout.CENTER);
-
-        this.currentLocationBackgroundIcon = getBackgroundImage();
-        this.imageLabel.setIcon(currentLocationBackgroundIcon);
-        this.imageLabel.setBounds(50, 0, 500, 500);
-
-        this.imagePanel.add(this.imageLabel, BorderLayout.CENTER);
-
+        currentLocationBackgroundIcon = getBackgroundImage();
+        backgroundImageJLabel.setIcon(currentLocationBackgroundIcon);
+        backgroundImageJLabel.setBounds(0, 0, 500, 500);
         // Add the panels to the middleHUD
+        imagePanel.add(backgroundImageJLabel, BorderLayout.CENTER);
         buttonPanel.add(this.imagePanel, BorderLayout.CENTER);
-        this.middleSectionPanel.add(buttonPanel, BorderLayout.CENTER);
+
+        if (game.getCurrentRoom().getEncounters_to().size() > 0) {
+            currentLocationEncounterIcon = getEncounterImageTo();
+            encounterLabel.setIcon(currentLocationEncounterIcon);
+            encounterLabel.setBounds(0, 0, 250, 250);
+            imagePanel.add(encounterLabel, BorderLayout.CENTER);
+            buttonPanel.add(imagePanel, BorderLayout.CENTER);
+        }
+        //TODO: check from encounters same way
+        middleSectionPanel.add(buttonPanel, BorderLayout.CENTER);
 
         // Set panel properties
         this.middleSectionPanel.setSize(500, 500);
@@ -121,10 +122,10 @@ public class MiddleSection {
 
     public ImageIcon getBackgroundImage() {
         String currentLocationImage = game.getCurrentRoom().getImage();
-        ImageIcon currentLocationBackgroundIcon = null;
+        currentLocationBackgroundIcon = null;
         try {
             currentLocationBackgroundIcon = new ImageIcon(ImageIO.read(Objects.requireNonNull
-                    (RunGUI.class.getClassLoader().getResourceAsStream(currentLocationImage))));
+                    (GUIClient.class.getClassLoader().getResourceAsStream(currentLocationImage))));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -132,37 +133,18 @@ public class MiddleSection {
     }
 
     public ImageIcon getEncounterImageTo() {
-        ImageIcon currentLocationEncounterIcon = new ImageIcon();
-
-        Room currentRoom = game.getCurrentRoom();
-        List<String> encounterToList = currentRoom.getEncounters_to();
-        if(encounterToList.isEmpty()) {
-            return null;
+        String encounter = game.getCurrentRoom().getEncounters_to().get(0);
+        String encounterImagePath = game.getEncounters().get(encounter).getImage();
+        currentLocationEncounterIcon = null;
+        try {
+            //DONE: inputStream is returning null
+            currentLocationEncounterIcon = new ImageIcon(ImageIO.read(Objects.requireNonNull
+                    (GUIClient.class.getClassLoader().getResourceAsStream(encounterImagePath))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NullPointerException e) {
+            System.out.println(e);
         }
-
-        String currentEncounter = encounterToList.get(0);
-        HashMap<String, Encounter> mapOfEncounters = game.getEncounters();
-
-        Encounter encounter = null;
-        String encounterImagePath = null;
-
-        if( mapOfEncounters.get(currentEncounter) != null ) {
-            encounter = mapOfEncounters.get(currentEncounter);
-        }
-
-        if( encounter != null ){
-            encounterImagePath = encounter.getImage();
-            System.out.println(encounterImagePath);
-            ImageIcon imageIcon = null;
-            try {
-                imageIcon = new ImageIcon(ImageIO.read(Objects.requireNonNull(MiddleSection.class.getClassLoader().getResourceAsStream(encounterImagePath))));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-                //TODO: Fix null pointer
-            } catch (NullPointerException ignored) {}
-            currentLocationEncounterIcon = imageIcon;
-        }
-
         return currentLocationEncounterIcon;
     }
 
@@ -171,7 +153,7 @@ public class MiddleSection {
 
         Room currentRoom = game.getCurrentRoom();
         List<String> encounterFromList = currentRoom.getEncounters_from();
-        if(encounterFromList.isEmpty()) {
+        if (encounterFromList.isEmpty()) {
             return null;
         }
 
@@ -181,22 +163,22 @@ public class MiddleSection {
         Encounter encounter = null;
         String encounterImagePath = null;
 
-        if( mapOfEncounters.get(currentEncounter) != null ) {
+        if (mapOfEncounters.get(currentEncounter) != null) {
             encounter = mapOfEncounters.get(currentEncounter);
         }
 
-        if( encounter != null ){
+        if (encounter != null) {
             encounterImagePath = encounter.getImage();
             System.out.println(encounterImagePath);
             try {
-                ImageIcon imageIcon = new ImageIcon(ImageIO.read(Objects.requireNonNull(MiddleSection.class.getClassLoader().getResourceAsStream(encounterImagePath))));
+                ImageIcon imageIcon = new ImageIcon(ImageIO.read(Objects.requireNonNull(RunGUI.class.getClassLoader().getResourceAsStream(encounterImagePath))));
             } catch (IOException e) {
                 throw new RuntimeException(e);
                 //TODO: Fix null pointer
-            } catch (NullPointerException ignored) {}
+            } catch (NullPointerException ignored) {
+            }
             currentLocationEncounterIcon = new ImageIcon(encounterImagePath);
         }
-
         return currentLocationEncounterIcon;
     }
 }
