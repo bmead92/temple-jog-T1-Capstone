bd3dcf285e6052d955834abc0b2c915a99dbe3e1
Joe Savella <Christian.Savella@tlgcohort.com>
2023-02-23 14:03:23 -0800

    enemy encounter icon removed after successful attack AB#1356

--- src/main/java/com/game/templejog/gui/bottom/UseMenu.java
+++ src/main/java/com/game/templejog/gui/bottom/UseMenu.java
@@ -3,6 +3,7 @@ package com.game.templejog.gui.bottom;
 import com.game.templejog.Game;
 import com.game.templejog.Item;
 import com.game.templejog.gui.MainContainer;
+import com.game.templejog.gui.middle.MiddleSection;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,6 +15,10 @@ public class UseMenu {
         attackMenu.setBackground(new Color(129,255,217));
         JPanel bottomRightSectionJPanel = MainContainer.getBottomSection().getBottomRightSection().getBottomRightSectionJPanel();
         bottomRightSectionJPanel.removeAll();
+
+        MiddleSection middleSection = MainContainer.getMiddleSection();
+        JPanel middleSectionPanel = MainContainer.getMiddleSection().getMiddleSectionPanel();
+
         JTextArea encounterInformation = new JTextArea();
         encounterInformation.setLineWrap(true);
         encounterInformation.setWrapStyleWord(true);
@@ -46,6 +51,12 @@ public class UseMenu {
                         bottomRightSectionJPanel.add(attackMenu);
                         bottomRightSectionJPanel.validate();
                         bottomRightSectionJPanel.repaint();
+
+                        //update middlesection panel after successful attack
+                        middleSection.setUpMiddleSectionJPanel();
+                        middleSectionPanel.revalidate();
+                        middleSectionPanel.repaint();
+
                     }
                 });
                 attackSubMenu.add(buttonToAdd);
